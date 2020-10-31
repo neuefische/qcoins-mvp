07fde9710cacb92634948e9f4103a1162a000ec6
Jan Robert <janmartinrobert@gmail.com>
2020-10-31 18:31:56 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { Switch, Redirect, Route, useHistory } from 'react-router-dom'
 import EarnForm from './EarnForm'
 import useTransactions from './hooks/useTransactions'
 import useEarnings from './hooks/useEarnings'
+import Home from './Home'
 
 function App() {
   const [transactions, addTransaction] = useTransactions()
   const [earnings, addEarning] = useEarnings()
 
+  const history = useHistory()
+
   return (
-    <div>
-      <EarnForm onSave={addEarning} />
-    </div>
+    <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/earn-coins">
+        <EarnForm onSave={addEarning} onCancel={() => history.push('/home')} />
+      </Route>
+      <Route path="/">
+        <Redirect to="/home" />
+      </Route>
+    </Switch>
   )
 }
 
