fb76befab51ce1fce566f695e8186398fc92aa70
Jeremias Erbs <jerry@badabam.de>
2020-10-31 09:43:53 +0100

    Add transactions logic
    Add functions for localStorage
    Add hook to save transaction with timestamp

---
+++ src/hooks/useTransactions.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import loadFromLocal from '../services/loadFromLocal'
+import saveToLocal from '../services/saveToLocal'
+
+const STORAGE_KEY = 'qcoins-transactions'
+
+export default function useTransactions() {
+  const [transactions, setTransactions] = useState(
+    loadFromLocal(STORAGE_KEY) ?? []
+  )
+  useEffect(() => saveToLocal(STORAGE_KEY, transactions), [transactions])
+
+  function addTransaction(transaction) {
+    transaction = { ...transaction, timestamp: new Date().toISOString() }
+    setTransactions([...transactions, transaction])
+  }
+
+  return [transactions, addTransaction]
+}
