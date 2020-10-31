c5ff04d771f28a97a3eeb69560581615d145b197
macfrei <lene@7freiheiten.de>
2020-10-31 18:52:27 +0100

    Add routing to distribute-coins page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import EarnForm from './EarnForm'
 import useTransactions from './hooks/useTransactions'
 import useEarnings from './hooks/useEarnings'
 import Home from './Home'
-import { DistributeCoins } from './DistributeCoins'
+import  DistributeCoins  from './DistributeCoins'
 
 function App() {
   const [transactions, addTransaction] = useTransactions()
   const [earnings, addEarning] = useEarnings()
-
   const history = useHistory()
-
+  
+  
   return (
     <Switch>
       <Route path="/home">
         <Home />
       </Route>
       <Route path="/earn-coins">
-        <EarnForm onSave={addEarning} onCancel={() => history.push('/home')} />
+        <EarnForm onSave={handleSaveEarnings} onCancel={() => history.push('/home')} />
       </Route>
       <Route path="/distribute-coins">
         <DistributeCoins earnedCoins={earnings[earnings.length - 1]} />
@@ -27,6 +27,12 @@ function App() {
       </Route>
     </Switch>
   )
+  
+  function handleSaveEarnings(earning) {
+    addEarning(earning)
+    history.push('/distribute-coins')
+  }
+
 }
 
 export default App
--- src/DistributeCoins.js
+++ src/DistributeCoins.js
@@ -1,1 +1,105 @@
-export function DistributeCoins() {}
+
+export default function DistributeCoins({earnedCoins}) {
+
+    return(
+    <div>{earnedCoins.amount}</div>
+    )
+}
+
+// import { useState } from 'react'
+// import styled from 'styled-components/macro'
+// import AmountButton from './AmountButton'
+
+// const initialState = {
+//   amount: 0,
+//   description: '',
+//   transactionType: '',
+// }
+
+// const transactionTypes = [
+//   {
+//     type: 'Spend',
+//     color: 'orange'
+//   },
+//   {
+//     type: 'Grow',
+//     color: 'blue'
+//   },
+//   {
+//     type: 'Share',
+//     color: 'purple'
+//   }
+// ]
+
+// export default function DistributeCoins({ onNewTransaction, earnedCoins }) {
+//   const [transaction, setTransaction] = useState(initialState)
+
+//   return (
+//     <FormStyled onSubmit={handleSubmit}>
+//       <section>
+//         Type of transaction:
+//         <div>
+//         {transactionTypes.map(({type, color}) => {
+//           return <ButtonStyled key={type} type="button" color={color} onClick={() =>
+//             setTransaction({ ...transaction, transactionType: type })
+//           }>{type}</ButtonStyled>
+//         })}
+//         </div>
+//       </section>
+//       <section>
+//         Amount: 
+//       <div>
+//         <AmountButton onClick={() => setTransactionAmount(-1)} amount="-1" />
+//         <AmountButton onClick={() => setTransactionAmount(-10)} amount="-10" />
+//         <span name="amount" value={transaction.description} type="text">
+//         {transaction.amount}
+//         </span>
+//         <AmountButton onClick={() => setTransactionAmount(10)} amount="10" />
+//         <AmountButton onClick={() => setTransactionAmount(1)} amount="1" />
+//       </div>
+//       </section>
+//       <LabelStyled>
+//         Description
+//         <input name="description" type="text" onChange={handleChange} />
+//       </LabelStyled>
+//       <div>
+//         <button type="button">Cancel</button>
+//         <button>Save</button>
+//       </div>
+//     </FormStyled>
+//   )
+
+  
+//   function handleSubmit(event) {
+//     event.preventDefault()
+//     onNewTransaction(transaction)
+//     setTransaction(initialState)
+//   }
+  
+//   function handleChange(event) {
+//     const { name, value } = event.target
+//     setTransaction({
+//       ...transaction,
+//       [name]: value,
+//     })
+//   }
+
+//   function setTransactionAmount(amount) {
+//     setTransaction({ ...transaction, amount: transaction.amount + amount })
+//   }
+// }
+
+// const FormStyled = styled.form`
+//   padding: 20px;
+// `
+// const LabelStyled = styled.label`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: baseline;
+//   justify-content: space-between;
+// `
+// const ButtonStyled = styled.button`
+//   background: ${props => props.color ?? 'gray'};
+//   color: white;
+//   padding: 6px;
+// `
